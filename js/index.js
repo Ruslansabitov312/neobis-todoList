a39a6da29d69a1bd8db4ed314f3d1bff746b8b76
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2022-06-30 03:05:16 +0600

    Add todo-list

---
+++ js/index.js
@@ -0,0 +1,68 @@
+window.addEventListener('load', () => {
+  const form = document.querySelector('.header__new-todo-form')
+  const input = document.querySelector('.header__new-todo-input')
+  const submit = document.querySelector('.header__new-todo-submit')
+  const listEl = document.querySelector('.todo-list__tasks')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const task = input.value
+
+    if (!task) {
+      alert('Please fill out the task!')
+      return
+    }
+
+    const taskEl = document.createElement('div')
+    taskEl.classList.add('todo-list__task')
+
+    const taskContentEl = document.createElement('div')
+    taskContentEl.classList.add('todo-list__content')
+
+    taskEl.appendChild(taskContentEl)
+
+    const taskInputEl = document.createElement('input')
+    taskInputEl.type = 'text'
+    taskInputEl.classList.add('todo-list__text')
+    taskInputEl.value = task
+    taskInputEl.setAttribute('readonly', 'readonly')
+
+    taskContentEl.appendChild(taskInputEl)
+
+    const taskActionsEl = document.createElement('div')
+    taskActionsEl.classList.add('todo-list__actions')
+
+    const taskEditEl = document.createElement('button')
+    taskEditEl.classList.add('todo-list__edit-btn')
+    taskEditEl.innerHTML = 'Edit'
+
+    const taskDeleteEl = document.createElement('button')
+    taskDeleteEl.classList.add('todo-list__delete-btn')
+    taskDeleteEl.innerHTML = 'Delete'
+
+    taskActionsEl.appendChild(taskEditEl)
+    taskActionsEl.appendChild(taskDeleteEl)
+
+    taskEl.appendChild(taskActionsEl)
+
+    listEl.appendChild(taskEl)
+
+    input.value = ''
+
+    taskEditEl.addEventListener('click', () => {
+      if (taskEditEl.innerText.toLowerCase() === 'edit') {
+        taskInputEl.removeAttribute('readonly')
+        taskInputEl.focus()
+        taskEditEl.innerHTML = 'Save'
+      } else {
+        taskInputEl.setAttribute('readonly', 'readonly')
+        taskEditEl.innerText = 'Edit'
+      }
+    })
+
+    taskDeleteEl.addEventListener('click', () => {
+      listEl.removeChild(taskEl)
+    })
+  })
+})
